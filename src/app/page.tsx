7fb0f59d2ed030576f7dd435b3791c9ed545e942
Chris <3287968+cmaenner@users.noreply.github.com>
2025-04-05 00:25:04 -0400

    Added new header and sponsor button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,12 @@
 'use client'
-
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
-
-const navigation = [
-  { name: 'Details Coming Soon!', href: '#' },
-]
+import Header from '@/app/components/headers'
+import Link from 'next/link'
 
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   return (
     <div className="bg-black min-h-screen">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">BSidesPhilly</span>
-              <Image
-                alt=""
-                src="/bsidesphilly_logo.png"
-                className="h-8 w-auto"
-                width={600}
-                height={600}
-              />
-            </a>
-          </div>
-          {/* <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
-            </button>
-          </div> */}
-          {/* <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-white">
-                {item.name}
-              </a>
-            ))}
-          </div> */}
-          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-white">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div> */}
-        </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-700/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">BSidesPhilly</span>
-                <Image
-                  alt=""
-                  src="/bsidesphilly_logo.png"
-                  className="h-8 w-auto"
-                  width={300}
-                  height={300}
-                />
-              </a>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-900"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                {/* <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-gray-900"
-                  >
-                    Log in
-                  </a>
-                </div> */}
-              </div>
-            </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
-
+      <Header />
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="sm:mb-8 flex justify-center">
@@ -130,6 +36,14 @@ export default function Home() {
               <br />
               900 Packer Ave, Philadelphia, PA 19148
             </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/sponsor"
+                className="rounded-md bg-[#4CBB17] px-6 py-3 text-lg font-semibold text-white shadow-sm hover:bg-[#3da913] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#4CBB17]"
+              >
+                Become a Sponsor
+              </Link>
+            </div>
           </div>
         </div>
       </div>
